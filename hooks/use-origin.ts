ccadccbb5127a306245251c65f8343323ad80957
Nguyen Dinh Truong <nguyendinhtruong98@gmail.com>
2024-02-04 10:45:45 +0700

    Completed app

---
+++ hooks/use-origin.ts
@@ -0,0 +1,8 @@
+export default function useOrigin() {
+  const origin =
+    typeof window !== "undefined" && window.location.origin
+      ? window.location.origin
+      : "";
+
+  return origin;
+}
